af614cc1855093549e7cd9f1eed1cedbdc858be1
Matija Kozelj <matija.kozelj.91@gmail.com>
2011-10-13 16:47:41 +0200

    Test commit

---
+++ Hello_application/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_hello = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_hello
+        '
+        Me.btn_hello.Location = New System.Drawing.Point(12, 12)
+        Me.btn_hello.Name = "btn_hello"
+        Me.btn_hello.Size = New System.Drawing.Size(75, 23)
+        Me.btn_hello.TabIndex = 0
+        Me.btn_hello.Text = "SayHello"
+        Me.btn_hello.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(116, 48)
+        Me.Controls.Add(Me.btn_hello)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btn_hello As System.Windows.Forms.Button
+
+End Class
---
+++ Hello_application/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub btn_hello_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_hello.Click
+        MessageBox.Show("Hello")
+    End Sub
+End Class
